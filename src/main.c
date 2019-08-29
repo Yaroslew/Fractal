10c64ec9db219c79735e64dde70deaeab9a3b81f
Yaroslew <Yarosleu@gmail.com>
2019-08-29 19:47:55 +0300

    pthread - первичное отображение

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: pcorlys- <pcorlys-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/06 17:06:53 by pcorlys-          #+#    #+#             */
-/*   Updated: 2019/08/29 19:01:17 by pcorlys-         ###   ########.fr       */
+/*   Updated: 2019/08/29 19:47:32 by pcorlys-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,13 +43,11 @@ static	int	mouse_hook(int button, int x, int y, t_base *base)
 	return (0);
 }
 
-static void	*start_fractal(void *str)
+static void	*start_fractal(void *param)
 {
 	t_base *base;
 
-	base = init_base(check((char*)str));
-	init_mlx(base);
-	init_fract(base);
+	base = param;
 	if (base->type_fract == 1)
 		mandelbrot(base);
 	if (base->type_fract == 2)
@@ -58,21 +56,32 @@ static void	*start_fractal(void *str)
 		mandelbar(base);
 
 	draw_img(base);
+	pthread_exit(0);
 
-	mlx_mouse_hook(base->mlx->win_ptr, mouse_hook, base);
-	mlx_hook(base->mlx->win_ptr, 2, 0, de_key, base);
-	mlx_loop(base->mlx->mlx_ptr);
 
 	return (NULL);
 }
 
 int main (int ar, char **av)
 {
-	pthread_t	tid;
+	pthread_t	first;
+	pthread_t	second;
+	t_base *base;
+
 
 	if (ar != 2)
 		mess_err(1);
-	pthread_create(&tid, NULL, start_fractal, ((void*)av[1]));
-	pthread_join(tid, NULL);
-	pthread_exit(0);
+	base = init_base(check(av[1]));
+	init_mlx(base);
+	init_fract(base);
+
+	base->height /= 2;
+	pthread_create(&first, NULL, start_fractal, ((void*)base));
+	base->fract->y = base->height;
+	base->height *= 2;
+	pthread_create(&second, NULL, start_fractal, ((void*)base));
+
+	mlx_mouse_hook(base->mlx->win_ptr, mouse_hook, base);
+	mlx_hook(base->mlx->win_ptr, 2, 0, de_key, base);
+	mlx_loop(base->mlx->mlx_ptr);
 }
